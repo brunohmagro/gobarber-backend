2ccd4bdf5bbb883c182da149eb655435ec785c4e
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-02-23 15:58:44 -0300

    Initializing image upload and files

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express'
+import multer from 'multer'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 import CreateUserServive from '../services/CreateUserService'
+import uploadConfig from '../config/upload'
 
 const usersRouter = Router()
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -22,5 +26,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message })
   }
 })
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  (request, response) => {
+    return response.json({ message: 'Deu certo acessar a rota' })
+  }
+)
 
 export default usersRouter
